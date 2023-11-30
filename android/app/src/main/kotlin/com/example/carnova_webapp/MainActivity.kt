aa750b8e0caf4721894d1ced06e10ac40cfb5756
Yaseeeeeen <96672031+yaseeeeeeeeeeen@users.noreply.github.com>
2023-11-30 09:50:41 +0530

    webapp-day1

---
+++ android/app/src/main/kotlin/com/example/carnova_webapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.carnova_webapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class ColorsModel {
+  static Color primary = const Color.fromARGB(255, 58, 77, 57);
+}
